0b82b1ee7919acc0b412d889d07d44f814e5a6ce
Jack Evans <jackevans38@gmail.com>
2020-06-29 12:44:03 +0100

    Init commit

---
+++ components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="header">
+		<ul class="dropdown">
+			<li>Icon</li>
+			<li>File</li>
+			<li>About</li>
+			<li>Themes</li>
+		</ul>
+	</div>
+</template>
+
+<style lang="scss">
+.header {
+	display: flex;
+	background: var(--color-grey);
+	height: 26px;
+	border-bottom: 2px solid var(--color-black);
+	padding: 0 1rem;
+	font-size: 16px;
+}
+
+.dropdown {
+	background: blue;
+	list-style: none;
+	display: grid;
+	grid-template-columns: repeat(4, 2fr);
+}
+</style>
